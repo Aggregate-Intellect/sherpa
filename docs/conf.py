65a8f95933691aed6618780d9350aa8fbea64533
Amir Feizpour <amir.fzpr@gmail.com>
2023-05-15 20:41:45 -0400

    pyramid theme

--- docs/conf.py
+++ docs/conf.py
@@ -47,8 +47,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinxawesome_theme"
-extensions = ["sphinxawesome_theme"]
+html_theme = "pyramid"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
