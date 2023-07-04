d7104c6115b153dae88fc6479b6b770786ef8353
YujingYang <yujing.yang2@mail.mcgill.ca>
2023-07-03 23:42:28 -0400

    add changes

---
+++ apps/slackbot/task_agent.py
@@ -0,0 +1,163 @@
+from typing import List, Optional
+
+from pydantic import ValidationError
+
+from langchain.chains.llm import LLMChain
+from langchain.chat_models.base import BaseChatModel
+from output_parser import TaskOutputParser, BaseTaskOutputParser
+from prompt import SlackBotPrompt
+from langchain.schema import Document
+from langchain.tools.base import BaseTool
+from langchain.tools.human.tool import HumanInputRun
+from langchain.vectorstores.base import VectorStoreRetriever
+from tools import UserInputTool
+
+
+class TaskAgent:
+    """Agent class for handling a single task"""
+
+    def __init__(
+        self,
+        ai_name: str,
+        memory: VectorStoreRetriever,
+        chain: LLMChain,
+        output_parser: BaseTaskOutputParser,
+        tools: List[BaseTool],
+        feedback_tool: Optional[HumanInputRun] = None,
+        max_iterations: int = 5,
+    ):
+        self.ai_name = ai_name
+        self.memory = memory
+        # self.full_message_history: List[BaseMessage] = []
+        self.next_action_count = 0
+        self.chain = chain
+        self.output_parser = output_parser
+        self.tools = tools
+        self.feedback_tool = feedback_tool
+        self.max_iterations = max_iterations
+
+        print(self.full_message_history)
+
+    @classmethod
+    def from_llm_and_tools(
+        cls,
+        ai_name: str,
+        ai_role: str,
+        memory: VectorStoreRetriever,
+        tools: List[BaseTool],
+        llm: BaseChatModel,
+        human_in_the_loop: bool = False,
+        output_parser: Optional[BaseTaskOutputParser] = None,
+        max_iterations: int = 5,
+    ):
+        prompt = SlackBotPrompt(
+            ai_name=ai_name,
+            ai_role=ai_role,
+            tools=tools,
+            # input_variables=["memory", "messages", "user_input", "task"],
+            token_counter=llm.get_num_tokens,
+        )
+        human_feedback_tool = HumanInputRun() if human_in_the_loop else None
+        chain = LLMChain(llm=llm, prompt=prompt)
+        return cls(
+            ai_name,
+            memory,
+            chain,
+            output_parser or TaskOutputParser(),
+            tools,
+            feedback_tool=human_feedback_tool,
+            max_iterations=max_iterations,
+        )
+
+    def run(self, task: str) -> str:
+        user_input = (
+            "Determine which next command to use, "
+            "and respond using the JSON format specified above without any extra text."
+            "\n JSON Response: \n"
+        )
+
+        # Interaction Loop
+        while True:
+            # Discontinue if continuous limit is reached
+            loop_count = self.logger.get_count()
+            print(f"Step: {loop_count}/{self.max_iterations}")
+
+            if loop_count >= self.max_iterations:
+                user_input = (
+                    "Use information gathered above to finish the task."
+                    "if the tool used is Search Tool, create inline citation at the of the sentence that use the result of the Search Tool"
+                    "Give a number of citation and put the link from result of a search tool at each inline citation"
+                    "only write text but not the JSON format specified above. \nResult:"
+                )
+
+            # Send message to AI, get response
+            assistant_reply = self.chain.run(
+                task=task,
+                messages=self.full_message_history,
+                memory=self.memory,
+                user_input=user_input,
+            )
+
+            # return if maximum itertation limit is reached
+            if loop_count >= self.max_iterations:
+
+                # TODO: this should be handled better, e.g. message for each task
+                self.logger.session.context["full_messages"] = []
+                self.logger.session.save()
+
+                self.logger.log(FinishLog(content=assistant_reply))
+
+                return assistant_reply
+
+            # Get command name and arguments
+            action = self.output_parser.parse(assistant_reply)
+            tools = {t.name: t for t in self.tools}
+            if action.name == "finish":
+                self.logger.set_count(self.max_iterations)
+                result = "Finished task. "
+            elif action.name in tools:
+                tool = tools[action.name]
+
+                if tool.name == "UserInput":
+                    return {'type': 'user_input', 'query': action.args['query']}
+
+                try:
+                    observation = tool.run(action.args)
+                except ValidationError as e:
+                    observation = (
+                        f"Validation Error in args: {str(e)}, args: {action.args}"
+                    )
+                except Exception as e:
+                    observation = (
+                        f"Error: {str(e)}, {type(e).__name__}, args: {action.args}"
+                    )
+                result = f"Command {tool.name} returned: {observation}"
+            elif action.name == "ERROR":
+                result = f"Error: {action.args}. "
+            else:
+                result = (
+                    f"Unknown command '{action.name}'. "
+                    f"Please refer to the 'COMMANDS' list for available "
+                    f"commands and only respond in the specified JSON format."
+                )
+
+            memory_to_add = (
+                f"Assistant Reply: {assistant_reply} " f"\nResult: {result} "
+            )
+            if self.feedback_tool is not None:
+                feedback = f"\n{self.feedback_tool.run('Input: ')}"
+                if feedback in {"q", "stop"}:
+                    print("EXITING")
+                    return "EXITING"
+                memory_to_add += feedback
+
+            self.memory.add_documents([Document(page_content=memory_to_add)])
+
+    def set_user_input(self, user_input: str):
+        result = f"Command UserInput returned: {user_input}"
+        assistant_reply = self.logger.get_full_messages()[-1].content
+        memory_to_add = (
+                f"Assistant Reply: {assistant_reply} " f"\nResult: {result} "
+        )
+
+        self.memory.add_documents([Document(page_content=memory_to_add)])
---
+++ apps/slackbot/tools.py
@@ -0,0 +1,153 @@
+from typing import Any
+from langchain import LLMChain
+from langchain.tools import BaseTool
+import requests
+from bs4 import BeautifulSoup
+from langchain.utilities import GoogleSerperAPIWrapper
+from langchain.docstore.document import Document
+from langchain.prompts import Prompt
+from langchain.vectorstores.base import VectorStoreRetriever
+from typing_extensions import Literal
+
+
+def get_tools(memory):
+    prompt = (
+        "You are an assistant helping user solve the task. Perform the task as writen"
+        "in the instruction.\nTask: {input}\nResult: "
+    )
+    prompt = Prompt.from_template(prompt)
+    # llm_chain = LLMChain(llm=llm, prompt=prompt)
+    search_tool = SearchTool(api_wrapper=GoogleSerperAPIWrapper())
+    # llm_tool = LLMTool(llm_chain=llm_chain)
+
+    user_input_tool = UserInputTool()
+    context_tool = ContextTool(memory=memory)
+
+    return [search_tool, context_tool, user_input_tool]
+
+
+class ScrapeTool(BaseTool):
+    name = "Scrape"
+    description = "A tool for scraping a website for information."
+    chunk_size = 200
+
+    def _run(self, url: str) -> str:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        data = soup.get_text(strip=True)
+
+        return data
+
+    def _arun(self, *args: Any, **kwargs: Any):
+        raise NotImplementedError("ScrapeTool does not support async run")
+
+
+class SearchTool(BaseTool):
+    name = "Search"
+    description = (
+        "Access the internet to search for the information, the input is a search query"
+    )
+    api_wrapper: GoogleSerperAPIWrapper
+
+    def _run(self, query: str) -> str:
+        google_serper = GoogleSerperAPIWrapper()
+        search_results = google_serper._google_serper_api_results(query)
+      
+        # case 1: answerBox in the result dictionary
+        if search_results.get("answerBox"):
+            answer_box = search_results.get("answerBox", {})
+            if answer_box.get("answer"):
+                answer = answer_box.get("answer")
+            elif answer_box.get("snippet"):
+                answer = answer_box.get("snippet").replace("\n", " ")
+            elif answer_box.get("snippetHighlighted"):
+                answer = answer_box.get("snippetHighlighted")
+            title = search_results["organic"][0]['title']
+            link = search_results["organic"][0]['link']
+            return "Answer: " + answer + "\nTitle:" + title + "\nLink:" + link
+      
+      # case 2: knowledgeGraph in the result dictionary
+        snippets = []
+        if search_results.get("knowledgeGraph"):
+            kg = search_results.get("knowledgeGraph", {})
+            title = kg.get("title")
+            entity_type = kg.get("type")
+            if entity_type:
+                snippets.append(f"{title}: {entity_type}.")
+            description = kg.get("description")
+            if description:
+                snippets.append(description)
+            for attribute, value in kg.get("attributes", {}).items():
+                snippets.append(f"{title} {attribute}: {value}.")
+        k = 10
+        search_type: Literal["news", "search", "places", "images"] = "search"
+        result_key_for_type = {
+        "news": "news",
+        "places": "places",
+        "images": "images",
+        "search": "organic",
+        }
+        for result in search_results[result_key_for_type[search_type]][:k]:
+            if "snippet" in result:
+                snippets.append(result["snippet"])
+            for attribute, value in result.get("attributes", {}).items():
+                snippets.append(f"{attribute}: {value}.")
+
+            if len(snippets) == 0:
+                return ["No good Google Search Result was found"]
+
+        answer = " ".join(snippets)
+        title = search_results["organic"][0]['title']
+        link = search_results["organic"][0]['link']
+        return "Answer: " + answer + "\nTitle:" + title + "\nLink:" + link
+
+    def _arun(self, query: str) -> str:
+        raise NotImplementedError("SearchTool does not support async run")
+
+
+class LLMTool(BaseTool):
+    name = "LLM"
+    description = (
+        "Access the LLM to perform different tasks"
+    )
+    llm_chain: LLMChain
+
+    def _run(self, query: str) -> str:
+        return self.llm_chain.run(input=query)
+
+    def _arun(self, query: str) -> str:
+        raise NotImplementedError("LLMTool does not support async run")
+
+
+class ContextTool(BaseTool):
+    name = "Context"
+    description = (
+        "Access the read-only domain specific internal documents for the task."
+        "You use this tool if you need further clarification of the task."
+    )
+    memory: VectorStoreRetriever
+
+    def _run(self, query: str) -> str:
+        docs = self.memory.get_relevant_documents(query)
+        result = ""
+        for doc in docs:
+            result += doc.page_content + "\n"
+
+        return result
+
+    def _arun(self, query: str) -> str:
+        raise NotImplementedError("ContextTool does not support async run")
+
+
+class UserInputTool(BaseTool):
+    name = "UserInput"
+    description = (
+        "Access the user input for the task."
+        "You use this tool if you need further clarification of the task from the user."
+    )
+
+    def _run(self, query: str) -> str:
+        return input(query)
+
+    def _arun(self, query: str) -> str:
+        raise NotImplementedError("UserInputTool does not support async run")
