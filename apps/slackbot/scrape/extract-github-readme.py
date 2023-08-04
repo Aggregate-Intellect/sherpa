3a1e8174b00fc0670576f3601312f817aa39eda7
Sami Negash <smlnegash@gmail.com>
2023-08-04 15:38:58 +0300

    extraction of data from github api

--- apps/slackbot/scrape/extract-github-readme.py
+++ apps/slackbot/scrape/extract-github-readme.py
@@ -19,18 +19,18 @@ def get_owner_and_repo(url):
     url_content_list = url.split('/')
     return url_content_list[-2], url_content_list[-1]
 
-def extract_github_readme(url):
+def extract_github_readme(repo_url):
     pattern = r"(?:https?://)?(?:www\.)?github\.com/.*"
-    match = re.match(pattern, url)
+    match = re.match(pattern, repo_url)
     if(match):
-        owner, repo = get_owner_and_repo(url)
+        owner, repo = get_owner_and_repo(repo_url)
         path = "README.md"
         token = api_key
         print(token)
-        url = f"https://api.github.com/repos/{owner}/{repo}/contents"
+        github_api_url = f"https://api.github.com/repos/{owner}/{repo}/contents"
         headers = {"Authorization": f"token {token}", 'X-GitHub-Api-Version': '2022-11-28'}
 
-        response = requests.get(url, headers=headers)
+        response = requests.get(github_api_url, headers=headers)
        
         files = response.json()
         matching_files = [file['name'] for file in files if (
@@ -39,25 +39,30 @@ def extract_github_readme(url):
             file['name'].lower().startswith('readme')
         )]
         path = matching_files[0]
-        url = f"https://api.github.com/repos/{owner}/{repo}/contents/{path}"
+        github_api_url = f"https://api.github.com/repos/{owner}/{repo}/contents/{path}"
         headers = {"Authorization": f"token {token}", 'X-GitHub-Api-Version': '2022-11-28'}
 
-        response = requests.get(url, headers=headers)
+        response = requests.get(github_api_url, headers=headers)
         data = response.json()
         if "content" in data:
             content = base64.b64decode(data["content"]).decode("utf-8")
+            metadata =  [{
+                'type': 'github',
+                'url': repo_url
+            }]
+            save_to_pine_cone(content, metadata)
             return content
         else:
             print("README file not found.")
 
 
-def save_to_pine_cone(content):
+def save_to_pine_cone(content,metadatas):
     pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENV)
     index = pinecone.Index("langchain")
     embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
 
     vectorstore = ConversationStore("Github_data", index, embeddings, 'text')
-    vectorstore.add_texts(content)
+    vectorstore.add_texts(content,metadatas)
 
 
 extract_github_readme('https://github.com/TowhidKashem/snapchat-clone')
