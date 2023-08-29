93a9f19ffaf2c3d0a19ff46a94169b45d246f3f5
Boqi Chen <boqi.chen@mail.mcgill.ca>
2023-08-29 15:17:41 -0400

    create verbose logger template

---
+++ apps/slackbot/verbose_loggers/__init__.py
@@ -0,0 +1,11 @@
+from verbose_loggers.verbose_loggers import (
+    DummyVerboseLogger,
+    SlackVerboseLogger,
+    StorageVerboseLogger,
+)
+
+__all__ = [
+    "DummyVerboseLogger",
+    "SlackVerboseLogger",
+    "StorageVerboseLogger",
+]
