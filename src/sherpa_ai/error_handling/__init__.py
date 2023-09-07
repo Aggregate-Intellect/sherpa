d5c33c2ad90ef419173e1a2572c856e7097cec4d
Boqi Chen <boqi.chen@mail.mcgill.ca>
2023-09-07 12:01:19 -0400

    Rename data_handling module

---
+++ src/sherpa_ai/error_handling/__init__.py
@@ -0,0 +1,3 @@
+from sherpa_ai.error_handling.agent_error_handler import AgentErrorHandler
+
+__all__ = ["AgentErrorHandler"]
--- src/sherpa_ai/error_hanlding/__init__.py
+++
@@ -1,3 +0,0 @@
-from sherpa_ai.error_hanlding.agent_error_handler import AgentErrorHandler
-
-__all__ = ["AgentErrorHandler"]
