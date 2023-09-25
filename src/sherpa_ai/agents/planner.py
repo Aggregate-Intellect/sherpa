444d1c2a207b55baafccd2048aaeec927d552e6c
Boqi Chen <boqi.chen@mail.mcgill.ca>
2023-09-25 17:07:43 -0400

    Format code

--- src/sherpa_ai/agents/planner.py
+++ src/sherpa_ai/agents/planner.py
@@ -1,8 +1,10 @@
+from langchain.llms.base import LLM
+
+from sherpa_ai.actions.planning import TaskPlanning
 from sherpa_ai.agents.agent_pool import AgentPool
 from sherpa_ai.agents.base import BaseAgent
-from sherpa_ai.actions.planning import TaskPlanning
-from langchain.llms.base import LLM
 from sherpa_ai.memory.events import EventType
+
 
 class Planner(BaseAgent):
     def __init__(
@@ -10,11 +12,11 @@ class Planner(BaseAgent):
         name: str,
         description: str,
         agent_pool: AgentPool,
-        shared_memory=None, 
+        shared_memory=None,
         belief=None,
         action_selector=None,
         num_runs=1,
-        llm = LLM,
+        llm=LLM,
     ):
         # TODO: Define agent actions (planning), create necessary helper methods
         self.name = name
@@ -24,31 +26,35 @@ class Planner(BaseAgent):
         self.belief = belief
         self.action_selector = action_selector
         self.num_runs = num_runs
-        
+
         self.planning = TaskPlanning(llm)
-        
-        # raise NotImplementedError
 
     def plan(self, task: str):
         """
         Plan the next action for the agent pool
         """
         # TODO: save task and agent pool description to shared memory
-        
-        self.shared_memory.add(event_type = EventType.planning, agent = self.name, content = task)
+
+        self.shared_memory.add(
+            event_type=EventType.planning, agent=self.name, content=task
+        )
         agent_pool_description = self.agent_pool.get_agent_pool_description()
-        
+
         # TODO: why do we need to add this? This seems to be a belief or observation
-        self.shared_memory.add(event_type = EventType.planning, agent = "Agent pool description", content = agent_pool_description)
-        
+        self.shared_memory.add(
+            event_type=EventType.planning,
+            agent="Agent pool description",
+            content=agent_pool_description,
+        )
+
         # run the planning
         plan = self.planning.execute(task, agent_pool_description)
-        
+
         print(str(plan))
-        
+
         for step in plan.steps:
-            self.shared_memory.add(event_type = EventType.task, agent = step.agent_name , content = step.task)
-            
+            self.shared_memory.add(
+                event_type=EventType.task, agent=step.agent_name, content=step.task
+            )
+
         return plan
-        # TODO: return result
-        raise NotImplementedError
