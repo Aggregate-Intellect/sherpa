c0a61d8f99d7a3593ef3e50281bb845769eb148c
YujingYang <yujing.yang2@mail.mcgill.ca>
2023-10-23 18:26:21 -0400

    add initial code for task agent

--- src/sherpa_ai/agents/task_agent.py
+++ src/sherpa_ai/agents/task_agent.py
@@ -1,4 +1,7 @@
+import json
 from typing import List
+from loguru import logger
+from pydantic import ValidationError
 
 from langchain.schema import (
     AIMessage,
@@ -18,18 +21,19 @@ from sherpa_ai.agents.planner import Planner
 from sherpa_ai.agents.base import BaseAgent
 from sherpa_ai.orchestrator import Orchestrator, OrchestratorConfig
 from sherpa_ai.events import EventType
+from sherpa_ai.output_parser import BaseTaskOutputParser, TaskOutputParser
+from sherpa_ai.reflection import Reflection
+
+from langchain.base_language import BaseLanguageModel
+from langchain.tools.base import BaseTool
+
+from sherpa_ai.action_planner import SelectiveActionPlanner
+from sherpa_ai.output_parsers import LinkParser
 
 TASK_AGENT_DESRIPTION = """ You are a **task management assistant** who solves user questions and offers a detailed solution.
-You take the task as input and organize other agents to finish the task step by step.
-Other agents includes a machine learning engineer, a physicist, a task planner, and a user.
 """
 
 OBJECTIVE_PROMPT = """ Use tools and resource to respond to the user's message and solve any questions the user may have.
-"""
-
-USER_DESCRIPTION = """ This user need help with a specific task. He needs accurate answer, detailed explanation, and strong evidence.
-He is skilled in computer science and machine learning. He has knowledge, expertise, and industrial practice within these area.
-He can answer questions about what the user might need from the question and about topics in computer science and machine learning.
 """
 
 class TaskAgent(BaseAgent):
@@ -38,11 +42,13 @@ class TaskAgent(BaseAgent):
     """
     def __init__(
             self, 
+            llm: BaseLanguageModel,
+            tools: List[BaseTool],
+            output_parser: BaseTaskOutputParser,
             name: str = "Task Agent", 
             description: str = TASK_AGENT_DESRIPTION, 
             shared_memory=None, 
             belief=None,
-            action_selector=None,
             num_runs=3,
             previous_messages=None,
         ):
@@ -50,65 +56,113 @@ class TaskAgent(BaseAgent):
             self.description = description
             self.shared_memory = shared_memory
             self.belif = belief
+            self.num_runs = num_runs
+            self.loop_count = 0
+            self.tools = tools
+            self.llm = llm
+            self.previous_messages = previous_messages
+            self.action_planner = SelectiveActionPlanner(
+                self.llm, tools, ai_name=name, ai_role=TASK_AGENT_DESRIPTION
+            )
+            self.output_parser = output_parser or TaskOutputParser()
+            link_parser = LinkParser()
+            self.tool_output_parsers = [link_parser]
+
     
-    def process_chat_history(self, messages: List[dict]) -> List[BaseMessage]:
-        results = []
-
-        for message in messages:
-            if message["type"] != "message" and message["type"] != "text":
-                continue
-
-            message_cls = AIMessage if message["user"] == self.ai_id else HumanMessage
-            # replace the at in the message with the name of the bot
-            text = message["text"].replace(f"@{self.ai_id}", f"@{self.ai_name}")
-            # added by JF
-            text = text.split("#verbose", 1)[0]  # remove everything after #verbose
-            text = text.replace("-verbose", "")  # remove -verbose if it exists
-            results.append(message_cls(content=text))
-
-        return results
     """
     This function takes a task from user, organized agents to generate a solution for the user
     """
     def run(self, task: str) -> str:
-        config = OrchestratorConfig(llm_name='gpt-3.5-turbo')
-        orchestrator = Orchestrator(config=config)
+        reflection = Reflection(self.llm, self.tools, [])
+        loop_count = self.loop_count
+        if loop_count >= self.num_runs:
+            user_input = (
+                        "Use the above information to solve the task for the user:"
+                        f"\n{task}\n\n"
+                        "If you use any resource, then create inline citation by adding "
+                        " of the reference document at the end of the sentence in the format "
+                        "of 'Sentence [DocID]'\n"
+                        "Example:\n"
+                        "Sentence1 [1]. Sentence2. Sentence3 [2].\n"
+                        "Only use the reference document. DO NOT use any links"
+                        " DO NOT include citation if the resource is not"
+                        "necessary. only write text but not the JSON format specified "
+                        "above. \nResult:"
+                    )
+            assistant_reply = self.action_planner.select_action(
+                    self.previous_messages, self.shared_memory, task=task, user_input=user_input
+                )
+            result = ""
+            if loop_count >= self.num_runs:
+                try:
+                    result = json.loads(assistant_reply)
+                    if (
+                        "command" in result
+                        and "args" in result["command"]
+                        and "response" in result["command"]["args"]
+                    ):
+                        result = result["command"]["args"]["response"]
+                except json.JSONDecodeError:
+                    result = assistant_reply
 
-        # initialize shared memory
-        objective = OBJECTIVE_PROMPT + task
-        shared_memory = SharedMemory(objective=objective)
-        orchestrator.shared_memory = shared_memory
+                for output_parser in self.output_parser:
+                    result = output_parser.parse_output(result)
+                return result
+            
+            action = self.output_parser.parse(assistant_reply)
+            tools = {t.name: t for t in self.tools}
+            new_reply = reflection.evaluate_action(
+                action, assistant_reply, task, self.previous_messages
+            )
+            action = self.output_parser.parse(new_reply)
+            if action.name == "finish":
+                self.loop_count = self.num_runs
+                result = "Finished task. "
+            elif action.name in tools:
+                tool = tools[action.name]
 
-        # initialize agents
-        physicist = Physicist(llm=orchestrator.llm, shared_memory=shared_memory)    # do we really need the physicist for this case?
-        ml_engineer = MLEngineer(llm=orchestrator.llm, shared_memory=shared_memory)
-        user_agent = UserAgent(
-             name="user1",
-             description=USER_DESCRIPTION,
-             shared_memory=shared_memory
-        )
-        critic_agent = Critic(llm=orchestrator.llm, ratio=9, shared_memory=shared_memory)
+                if tool.name == "UserInput":
+                    return action.args["query"]
 
-        agent_pool = AgentPool()
-        agent_pool.add_agents([physicist, ml_engineer, user_agent])
+                try:
+                    observation = tool.run(action.args)
 
-        orchestrator.agent_pool = agent_pool
+                    for tool_output_parser in self.tool_output_parsers:
+                        observation = tool_output_parser.parse_output(
+                            observation, tool_output=True
+                        )
+                except ValidationError as e:
+                    observation = (
+                        f"Validation Error in args: {str(e)}, args: {action.args}"
+                    )
+                except Exception as e:
+                    observation = (
+                        f"Error: {str(e)}, {type(e).__name__}, args: {action.args}"
+                    )
+                result = f"Command {tool.name} returned: {observation}"
+            elif action.name == "ERROR":
+                result = f"Error: {action.args}. "
+            else:
+                result = (
+                    f"Unknown command '{action.name}'. "
+                    f"Please refer to the 'COMMANDS' list for available "
+                    f"commands and only respond in the specified JSON format."
+                )
+
+            self.loop_count += 1
+
+            memory_to_add = (
+                f"Assistant Reply: {assistant_reply} " f"\nResult: {result} "
+            )
+
+            self.previous_messages.append(HumanMessage(content=memory_to_add))
+
+
+
+
+         
+         
         
-        planner = Planner(
-            agent_pool=agent_pool,
-            shared_memory=shared_memory,
-            llm=orchestrator.llm,
-            num_steps=5,
-        )
-
-        # make plan
-        plan_text = orchestrator.plan(objective, planner, critic_agent)
-        plan = planner.planning.post_process(plan_text)
-        shared_memory.add(EventType.planning, "Planner", str(plan))
-        
-        # execute plan
-        result = orchestrator.execute(plan, planner)
-        return result
 
 
 
