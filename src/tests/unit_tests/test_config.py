c4a1ff1de123b089d07819fa5d93854c78fccab4
YujingYang <yujing.yang2@mail.mcgill.ca>
2023-10-31 21:56:50 -0400

    add gsite in the query

--- src/tests/unit_tests/test_config.py
+++ src/tests/unit_tests/test_config.py
@@ -1,6 +1,11 @@
 import pytest
 
 from sherpa_ai.config import AgentConfig
+from sherpa_ai.tools import ContextTool, SearchTool
+from langchain import GoogleSerperAPIWrapper
+from sherpa_ai.actions.google_search import GoogleSearch
+from langchain.llms import OpenAI
+import sherpa_ai.config as cfg
 
 
 def test_parse_args():
@@ -30,3 +35,27 @@ def test_parse_args_noise():
 
     with pytest.raises(ValueError):
         AgentConfig.from_input(input_str)
+
+
+def test_gsite_google_search():
+    site = "https://www.google.com"
+    input_str = f"Test input. --verbose --gsite {site}"
+
+    question, config = AgentConfig.from_input(input_str)
+
+    assert config.gsite == site
+
+    role_description =  "The programmer receives requirements about a program and write it"
+    
+    task = """We need to render a highly complex 3D image on the solar system. We can use any publicly avaliable
+    resources to achieve this task."""  # noqa: E501
+    
+    llm = OpenAI(openai_api_key=cfg.OPENAI_API_KEY, temperature=0)
+    
+    google_search = GoogleSearch(role_description=role_description, task=task, llm=llm, config=config)
+
+    query = "What is the weather today?"
+    
+    updated_query = google_search.config_gsite_query(query)
+    
+    assert site in updated_query
