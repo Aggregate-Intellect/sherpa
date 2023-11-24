d14193cf595fbbf514126bef730011b9e3126f2d
YujingYang <yujing.yang2@mail.mcgill.ca>
2023-11-24 14:50:40 -0500

    add four unit testing for search tool

--- src/tests/unit_tests/tools/test_search_tool.py
+++ src/tests/unit_tests/tools/test_search_tool.py
@@ -15,6 +15,62 @@ def test_search_query_includes_gsite_config():
 
     query = "What is the weather today?"
 
-    updated_query = search_tool.augment_query(query)
+    search_result = search_tool._run(query)
 
-    assert f"site:{site}" in updated_query
+    assert search_result is not None
+    assert search_result is not ""
+
+
+def test_search_query_includes_multiple_gsite_config():
+    site = "https://www.google.com, https://www.langchain.com/, https://openai.com/"
+    config = AgentConfig(
+        verbose=True,
+        gsite=site,
+    )
+
+    assert config.gsite == site
+
+    search_tool = SearchTool(config=config)
+
+    query = "What is the weather today?"
+
+    search_result = search_tool._run(query)
+
+    assert search_result is not None
+    assert search_result is not ""
+
+def test_search_query_includes_more_gsite_config_succeed():
+    site = "https://www.google.com, https://www.langchain.com/, https://openai.com/, https://www.google.com, https://www.langchain.com/, https://openai.com/"
+    config = AgentConfig(
+        verbose=True,
+        gsite=site,
+    )
+
+    assert config.gsite == site
+
+    search_tool = SearchTool(config=config)
+
+    query = "What is the weather today?"
+
+    search_result = search_tool._run(query)
+
+    assert "Warning: Only the first 5 URLs are taken into consideration." in search_result
+
+def test_search_query_includes_more_gsite_config_failed():
+    site = ""
+    config = AgentConfig(
+        verbose=True,
+        gsite=site,
+    )
+
+    assert config.gsite == site
+
+    search_tool = SearchTool(config=config)
+
+    query = "What is the weather today?"
+
+    search_result = search_tool._run(query)
+
+    assert search_result is not None
+    assert search_result is not ""
+
