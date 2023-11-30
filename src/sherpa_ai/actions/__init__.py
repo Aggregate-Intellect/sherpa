87ab4632894542a3a2e44944938f4fb9dad32fed
ChenKua <kua.chen@mail.mcgill.ca>
2023-11-29 20:49:48 -0500

    reorder to output parser

--- src/sherpa_ai/actions/__init__.py
+++ src/sherpa_ai/actions/__init__.py
@@ -2,12 +2,10 @@ from sherpa_ai.actions.deliberation import Deliberation
 from sherpa_ai.actions.google_search import GoogleSearch
 from sherpa_ai.actions.planning import TaskPlanning
 from sherpa_ai.actions.synthesize import SynthesizeOutput
-from sherpa_ai.actions.citation_validation import CitationValidation
 
 __all__ = [
     "Deliberation",
     "GoogleSearch",
     "TaskPlanning",
     "SynthesizeOutput",
-    "CitationValidation",
 ]
