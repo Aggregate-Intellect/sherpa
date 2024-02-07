a2268b16a068800f9754aaef769e5c02cbbc2458
Boqi Chen <boqi.chen@mail.mcgill.ca>
2024-02-06 23:14:35 -0500

    Fix extract readme bug and update other agents

--- src/sherpa_ai/scrape/extract_github_readme.py
+++ src/sherpa_ai/scrape/extract_github_readme.py
@@ -32,7 +32,8 @@ def extract_github_readme(repo_url):
         response = requests.get(github_api_url, headers=headers)
 
         files = response.json()
-        if type(files) is dict and files.message == "Bad Credentials":
+        logger.error(f"THis is files: {files}")
+        if type(files) is dict and files["message"].lower() == 'bad credentials':
             return None
         matching_files = [
             file["name"]
