7a035c7e900cda59822f166f279b3ec7bbd515e2
ChenKua <kua.chen@mail.mcgill.ca>
2024-02-08 18:05:44 -0500

    doc strings

--- src/sherpa_ai/output_parsers/__init__.py
+++ src/sherpa_ai/output_parsers/__init__.py
@@ -1,5 +1,6 @@
 from sherpa_ai.output_parsers.base import BaseOutputParser
+from sherpa_ai.output_parsers.citation_validation import CitationValidation
 from sherpa_ai.output_parsers.link_parse import LinkParser
 from sherpa_ai.output_parsers.md_to_slack_parse import MDToSlackParse
 
-__all__ = ["BaseOutputParser", "LinkParser", "MDToSlackParse"]
+__all__ = ["BaseOutputParser", "LinkParser", "MDToSlackParse", "CitationValidation"]
