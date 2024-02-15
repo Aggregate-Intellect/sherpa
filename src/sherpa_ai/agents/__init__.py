f9a715e3ad6ab9d014e0d4a9913ba288fc7af7e0
ChenKua <kua.chen@mail.mcgill.ca>
2024-02-15 08:51:38 -0500

    remove unnecessary files

--- src/sherpa_ai/agents/__init__.py
+++ src/sherpa_ai/agents/__init__.py
@@ -1,6 +1,5 @@
 from sherpa_ai.agents.agent_pool import AgentPool
 from sherpa_ai.agents.critic import Critic
-from sherpa_ai.agents.mathematician import Mathematician
 from sherpa_ai.agents.ml_engineer import MLEngineer
 from sherpa_ai.agents.physicist import Physicist
 from sherpa_ai.agents.planner import Planner
@@ -16,5 +15,4 @@ __all__ = [
     "UserAgent",
     "Critic",
     "QAAgent",
-    "Mathematician",
 ]
