2d916abf712b5f2023909283ca76f433a37c9d4c
ChenKua <kua.chen@mail.mcgill.ca>
2024-02-14 23:35:32 -0500

    add placeholder to class

--- src/tests/unit_tests/actions/test_answer_arithmetic.py
+++ src/tests/unit_tests/actions/test_answer_arithmetic.py
@@ -2,16 +2,22 @@ from sherpa_ai.actions import AnswerArithmetic
 
 
 def test_answer_arith():
-    m = AnswerArithmetic()
+    m = AnswerArithmetic(placeholder=False)
     question = (
         "if each apple costs 2 dollar. How much money do I need for buying 10 apples?"
     )
-
     answer = 20
     result = m.execute(question)
 
     assert answer == result
 
-    result = m.execute(question, True)
+
+def test_answer_arith_placeholder():
+    m = AnswerArithmetic(placeholder=True)
+    question = (
+        "if each apple costs 2 dollar. How much money do I need for buying 10 apples?"
+    )
+    answer = 20
+    result = m.execute(question)
 
     assert answer == result
