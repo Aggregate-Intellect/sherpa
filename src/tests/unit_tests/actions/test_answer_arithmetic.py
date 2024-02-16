12456fcdf6afd3daf5dce91e897c5c073615b6cb
ChenKua <kua.chen@mail.mcgill.ca>
2024-02-15 22:20:47 -0500

    offline tests.

--- src/tests/unit_tests/actions/test_answer_arithmetic.py
+++ src/tests/unit_tests/actions/test_answer_arithmetic.py
@@ -1,7 +1,11 @@
 from sherpa_ai.actions import AnswerArithmetic
+from sherpa_ai.test_utils.llms import get_llm
+import pytest
 
 
-def test_answer_arith():
+@pytest.mark.external_api
+def test_answer_arith(get_llm):
+    llm = get_llm(__file__, test_answer_arith.__name__)
     m = AnswerArithmetic(placeholder=False)
     question = (
         "if each apple costs 2 dollar. How much money do I need for buying 10 apples?"
@@ -12,7 +16,10 @@ def test_answer_arith():
     assert answer == result
 
 
-def test_answer_arith_placeholder():
+@pytest.mark.external_api
+def test_answer_arith_placeholder(get_llm):
+    llm = get_llm(__file__, test_answer_arith_placeholder.__name__)
+
     m = AnswerArithmetic(placeholder=True)
     question = (
         "if each apple costs 2 dollar. How much money do I need for buying 10 apples?"
