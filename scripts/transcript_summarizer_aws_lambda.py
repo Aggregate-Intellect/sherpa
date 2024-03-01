bacd9e21bcff4c2a83be9828bfdeccc5d3c43d30
Sami Negash <smlnegash@gmail.com>
2024-03-01 19:48:03 +0300

    rst file processing plus

--- scripts/transcript_summarizer_aws_lambda.py
+++ scripts/transcript_summarizer_aws_lambda.py
@@ -1,7 +1,20 @@
 import json
 import boto3
 import os
+
+from os import environ
+# destination_bucket = "transcriptslangchain"
 destination_bucket = os.environ['destination_bucket']
+
+
+
+
+ACCESS_KEY = environ.get('ACCESS_KEY')  
+SECRET_KEY = environ.get('SECRET_KEY') 
+REGION_NAME = environ.get('REGION_NAME')
+OPENAI_API_KEY = environ.get('OPENAI_API_KEY')
+
+
 import tiktoken
 from langchain.chat_models import ChatOpenAI
 from langchain import PromptTemplate, LLMChain
@@ -26,7 +39,7 @@ import json
 
 # instantiate chat model
 chat = ChatOpenAI(
-  openai_api_key=os.environ['openaikey'] ,
+  openai_api_key=OPENAI_API_KEY,
   temperature=0,
   model='gpt-3.5-turbo')
 
@@ -40,7 +53,6 @@ def num_tokens_from_string(string: str, encoding_name="cl100k_base") -> int:
   return num_tokens
 
 """## Splitting raw transcript
-
 ChatGPT models have a token limit. For GPT3.5-turbo, the limit is 4096 tokens [(docs)](https://platform.openai.com/docs/models/gpt-3-5). Most transcripts exceed that, so it must be split into chunks.
 """
 def transcript_splitter(raw_transcript, chunk_size=10000, chunk_overlap=200):
@@ -66,6 +78,42 @@ def transcript2essay(transcript, chat_model=chat):
   result = chat_model(chat_prompt.format_prompt(transcript=transcript).to_messages())
   return result.content
 
+
+
+def qa2essay_bkp(transcript,summarized_essay, chat_model=chat):
+  system_template = "You are a helpful assistant that summarizes a transcript of QA section of podcasts or lectures."
+  system_prompt = SystemMessagePromptTemplate.from_template(system_template)
+  # human_template = "Summarize the main points of this presentation's transcript: {transcript}"
+#   human_template = """Rewrite the contents and information of the presentation into a well written essay.\
+#   Write the essay as if the speaker wrote it himself from the same knowledge he used to create the presentation. \
+#   Include the speaker's full name in the essay and refer to him/her with the full name. \
+#   Also include the names of the people who asked questions and the questions they asked. \
+#   The transcript of this presentation is delimited in triple backticks:
+#   ```{transcript}```"""
+  human_template = """Rewrite the Questions and answers section of the presentation that outputs the same number of questions and answers in a summarized way.\
+  Make sure you keep the same number of question and answers. Assume that the only one asking questions is the host, and the only one answering is speakers.\
+  sometimes the same answer is broken down in few lines. Below is the question and answers section \
+  ```{transcript}``` The next section is the summarized section on which question and answers \
+  are been discussed, use this for summarizing the question and answers \
+  section """ + summarized_essay
+  human_prompt = HumanMessagePromptTemplate.from_template(human_template)
+  chat_prompt = ChatPromptTemplate.from_messages([system_prompt, human_prompt])
+  result = chat_model(chat_prompt.format_prompt(transcript=transcript).to_messages())
+  return result.content
+
+def qa2essay(transcript,summarized_essay, chat_model=chat):
+  system_template = "You are a helpful assistant that summarizes a transcript of QA section of podcasts or lectures."
+  system_prompt = SystemMessagePromptTemplate.from_template(system_template)
+  human_template = """ You are given a transcript that is a conversation of the question and answer. \
+  Create a list of question and answeres from it with the following instructions:\
+  Some questions might be spread across multiple lines, so summarise them.\
+  Do not use numbers instead use the format Question and Answer/speaker to list them down. \
+  The transcript of this presentation is delimited in triple backticks: ```{transcript}```Use the summary to elaborate on the answers """ + summarized_essay
+  human_prompt = HumanMessagePromptTemplate.from_template(human_template)
+  chat_prompt = ChatPromptTemplate.from_messages([system_prompt, human_prompt])
+  result = chat_model(chat_prompt.format_prompt(transcript=transcript).to_messages())
+  return result.content
+
 def create_essay_parts(transcript_docs,chat_model=chat):
   essay_response = ''
   for i, text in enumerate(transcript_docs):
@@ -74,6 +122,13 @@ def create_essay_parts(transcript_docs,chat_model=chat):
 
   return essay_response
 
+def create_qa_parts(transcript_docs,summarized_essay):
+  essay_response = ''
+  for i, text in enumerate(transcript_docs):
+      essay = qa2essay(text.page_content,summarized_essay)
+      essay_response = f'\n\n#Part {i+1}\n'.join([essay_response, essay])
+
+  return essay_response
 
 def merge_essays(essays, chat_model=chat):
   system_template = """You are a helpful assistant that summarizes and \
@@ -113,6 +168,53 @@ def full_transcript2essay(raw_transcript:str, chat_model=chat, verbose=True):
   return final_essay
 
 
+# @timer_decorator
+def full_qa2essay(raw_transcript:str, summarized_essay:str,chat_model=chat, verbose=True):
+  print('Chunking qa...')
+  transcript_docs = transcript_splitter(raw_transcript)
+  t1 = time.time()
+  print('Creating qa parts...')
+  qa_parts = create_qa_parts(transcript_docs,summarized_essay)#, chat_model=chat)
+  t2 = time.time()-t1
+#   print('Merging essay parts...')
+#   t1 = time.time()
+#   final_essay = merge_essays(essay_parts, chat_model=chat)
+#   t3 = time.time()-t1
+  if verbose:
+    print(f'Created qa parts in {t2:.2f} seconds')
+    #print(f'Merged essay parts in {t3:.2f} seconds')
+  return qa_parts
+
+
+# """# Part 2: Extracting from essay"""
+
+def extract_qa_metadata_as_json(essay, chat_model=chat):
+
+  system_template = """ Given the question answers section, \
+  Format the response as a JSON object, with the keys 'Question, 'Answer', 'Speaker', \
+  Example of JSON output: \n \
+  {{\
+  'Question': '',\
+  'Answer': 
+  }}"""
+    
+    
+  system_prompt = SystemMessagePromptTemplate.from_template(system_template)
+
+  human_template = """Essay: ```{text}```"""
+
+  human_prompt = HumanMessagePromptTemplate.from_template(human_template)
+  chat_prompt = ChatPromptTemplate.from_messages([system_prompt, human_prompt])
+
+  result = chat_model(chat_prompt.format_prompt(text=essay).to_messages())
+  try:
+    metadata_json = json.loads(result.content)
+  except Exception as e:
+    print(e)
+    metadata_json = result.content  
+  return metadata_json
+
+
 # """# Part 2: Extracting from essay"""
 
 def extract_metadata_as_json(essay, chat_model=chat):
@@ -121,12 +223,12 @@ def extract_metadata_as_json(essay, chat_model=chat):
   information such as the title, speaker, summary, a list of key topics, \
   and a list of important takeaways for each topic. \
   Format the response as a JSON object, with the keys 'Title', 'Topics', 'Speaker', \
-  'Essay', and 'Topics' as the keys and each topic will be keys for list of takeaways. \
+  'Summary', and 'Topics' as the keys and each topic will be keys for list of takeaways. \
   Example of JSON output: \n \
  {{\
   'Title': 'Title of the presentation',\
   'Speaker': 'John Smith',\
-  'Essay': 'summary of the presentation',\
+  'Summary': 'summary of the presentation',\
   'Topics': [\
   {{\
   'Topic': 'topic 1',\
@@ -162,11 +264,11 @@ def extract_metadata_as_json(essay, chat_model=chat):
   }}\
   ]\
   }}"""
-  
+
   system_prompt = SystemMessagePromptTemplate.from_template(system_template)
 
   human_template = """Essay: ```{text}```"""
-  
+
   human_prompt = HumanMessagePromptTemplate.from_template(human_template)
   chat_prompt = ChatPromptTemplate.from_messages([system_prompt, human_prompt])
 
@@ -187,6 +289,7 @@ def json2rst(metadata, rst_filepath):
   with open(rst_filepath, 'a') as the_file:
       the_file.write("\n\n")
       for key, value in metadata.items():
+          print("jsong2rst", key, value)
           if key == "Title":
               title_mark = "=" * len(f'{value}')
               the_file.write(title_mark + '\n')
@@ -194,9 +297,9 @@ def json2rst(metadata, rst_filepath):
               the_file.write(title_mark + '\n')
           elif key == "Speaker":
               the_file.write('*' + f"{value}" + '* \n\n')
-          elif key == "Essay":
+          elif key == "Summary":
               title_mark = '-' * len(f'{key}')
-              the_file.write("Essay \n")
+              the_file.write("Summary \n")
               the_file.write(title_mark + '\n')
               the_file.write(f"{value} \n\n")
           elif key == "Topics":
@@ -206,34 +309,42 @@ def json2rst(metadata, rst_filepath):
                   the_file.write("\t" + f"{topic['Topic']} \n")
                   for takeaway in topic['Takeaways']:
                       the_file.write("\t\t" + f"* {takeaway} \n")
+          elif key == "Question":
+              the_file.write("Question: \n")
+              the_file.write(title_mark + '\n')
+              the_file.write(f"{value}\n\n")
+          elif key == "Answer":
+              the_file.write("Answer: \n")
+              the_file.write(title_mark + '\n')
+              the_file.write(f"{value}\n\n")
 
 
 
 def lambda_handler(event,context):
   # TODO implement
-    
-    
-    
+
+
+
      #Retrieve the source and destination bucket names from the event
     source_bucket = event['Records'][0]['s3']['bucket']['name']
-    
+
     # Retrieve the key (filename) of the object that triggered the Lambda function
     source_key = event['Records'][0]['s3']['object']['key']
-    
-    
-    ACCESS_KEY	= os.environ['ACCESS_KEY'] 
-    SECRET_KEY	= os.environ['SECRET_KEY'] 
+
+
+    # ACCESS_KEY	= os.environ['ACCESS_KEY'] 
+    # SECRET_KEY	= os.environ['SECRET_KEY'] 
     #s3_client = boto3.client('s3')
     s3_client = boto3.client(
     's3',
     aws_access_key_id=ACCESS_KEY,
     aws_secret_access_key=SECRET_KEY
     )
-    
+
     print(source_bucket)
     print(source_key)
-    
-    
+
+
     # response = s3_client.get_object(Bucket=source_bucket, Key=source_key)
     # docx_file = response['Body'].read()
 
@@ -247,7 +358,7 @@ def lambda_handler(event,context):
     #     'statusCode': 200,
     #     'body': json.dumps(text)
     # }
-    
+
     # Read the file content from the source bucket
     response = s3_client.get_object(Bucket=source_bucket, Key=source_key)
     print("Response :",response)
@@ -258,16 +369,74 @@ def lambda_handler(event,context):
     except UnicodeDecodeError as e:
         print("In exception")
         raw_transcript = response['Body'].read().decode('utf-8', errors='replace')
-        
-    
-    print("Raw Transcript : ",raw_transcript)
-        # takes about 2-3 minutes to run
-    essay = full_transcript2essay(raw_transcript)
 
-    print("Summary : ",essay)
+
+    # qa_index = raw_transcript.find("## QUESTION ANSWERING SECTION")
+    
+    # Find the indices where each section starts
+    talk_index = raw_transcript.find("# TALK") + len("# TALK\n")
+    qa_index = raw_transcript.find("## QUESTION ANSWERING SECTION") + len("## QUESTION ANSWERING SECTION\n")
+    
+    # Extract each section using slicing
+    first_part = raw_transcript[talk_index:qa_index].strip()
+    second_part = raw_transcript[qa_index:].strip()
+
+    # first_part = raw_transcript[:qa_index]
+    # second_part = raw_transcript[qa_index:]
+
+    # print("Raw Transcript : ",raw_transcript)
+    print("----first_part : ",first_part)
+    print("---- Second part : ", second_part)
+    
+    
+        # takes about 2-3 minutes to run
+    #summary= full_transcript2essay(raw_transcript)
+    summary= full_transcript2essay(first_part) #considering only talk part
+
+    print("Summary : ",summary)
+    
+    qa_summarized = full_qa2essay(second_part,summary)
+    print("------ Second part : ",second_part)
+    print("------ QA Summarized : ",qa_summarized)
+    
+    
+     # save metadata to file
+    qa_filepath = r'/tmp/test_qa_summarized.txt'
+    with open(qa_filepath, 'w') as file:
+        json.dump(qa_summarized, file)
+    
+     # 17 seconds to run
+    print('Extracting metadata...')
+    qa_metadata = extract_qa_metadata_as_json(qa_summarized, chat_model=chat)
+
+    # save metadata to file
+    qa_metadata_filepath = r'/tmp/test_qa_metadata.json'
+    with open(qa_metadata_filepath, 'w') as file:
+        json.dump(qa_metadata, file)
+
+    print("--- qa_metadata", qa_metadata)
+    
+    # destination_key = source_key.split(".")[0] +"qa_summarized.txt"
+    # #destination_key = 'test_qa_summarized.txt'
+    # print("------ QA summarized filename : ",destination_key)
+    # s3_client.upload_file(qa_filepath, destination_bucket, destination_key)
+    
+    # SAM TODO : Convert this to rst format and wrtie into the same rst file
+    # take the input file with the format that we discussed - Done
+    # the talk part only should be summarized into take aways (currently exists in the output) and essay (exists in the code but not in the output) - Done
+    # the qa part should be summarized to one question and answer per interaction between the host and the speaker - Change qa2essay to match it
+    # all this should be added to the same output rst file  - Add the output to the rst file
+        
+    ##################################
+    
+    
+    
     # 17 seconds to run
     print('Extracting metadata...')
-    metadata = extract_metadata_as_json(essay, chat_model=chat)
+    metadata = extract_metadata_as_json(summary, chat_model=chat)
+
+    output_json = r'/tmp/output_json.json'
+
 
     # save metadata to file
     metadata_filepath = r'/tmp/test_metadata.json'
@@ -275,10 +444,33 @@ def lambda_handler(event,context):
         json.dump(metadata, file)
 
     print(metadata)
+
+    try:
+        with open(qa_metadata_filepath, 'r') as f:
+            source_data = json.load(f)
+
+        # Read content from target JSON file
+        with open(metadata_filepath, 'r') as f:
+            target_data = json.load(f)
+
+        # Merge dictionaries
+        merged_data = {**target_data, **source_data}
+
+        # Write merged data to output JSON file
+        with open(output_json, 'w') as f:
+            json.dump(merged_data, f, indent=4)
+
+        print(f"Content from '{metadata}' merged to '{qa_metadata}' successfully.")
+    except Exception as e:
+       pass
     # convert metadata from json to rst
     rst_filepath = r'/tmp/test.rst'
     try:
-      json2rst(metadata, rst_filepath)
+      output_to_json = ''
+      with open(output_json, 'r') as file:
+         output_to_json = json.load(file)
+      print("outputJson",output_json)
+      json2rst(output_to_json, rst_filepath)
       destination_key = source_key.split(".")[0] +".rst"
       #s3_client.upload_file(rst_filepath, destination_bucket, destination_key)
       file_path = rst_filepath
@@ -286,83 +478,84 @@ def lambda_handler(event,context):
       print("Parsing error from Json to Rst for file :", source_key)
       destination_key = source_key.split(".")[0] +".json"
       file_path = metadata_filepath
+
     
     s3_client.upload_file(file_path, destination_bucket, destination_key)
 
-    
+
     print(destination_key)
-    
-    
+
+
     return {
         'statusCode': 200,
         'body': json.dumps('Rst file comversion successful !!!')
     }
 
 
-def lambda_handler_bkp(event, context):
-    # TODO implement
-    
-     #Retrieve the source and destination bucket names from the event
-    source_bucket = event['Records'][0]['s3']['bucket']['name']
+# # def lambda_handler_bkp(event, context):
+#     # TODO implement
 
-    
-    # Retrieve the key (filename) of the object that triggered the Lambda function
-    source_key = event['Records'][0]['s3']['object']['key']
-    
-    
-    ACCESS_KEY	= os.environ['ACCESS_KEY'] 
-    SECRET_KEY	= os.environ['SECRET_KEY'] 
-    #s3_client = boto3.client('s3')
-    s3_client = boto3.client(
-    's3',
-    aws_access_key_id=ACCESS_KEY,
-    aws_secret_access_key=SECRET_KEY
-    )
-    
-    print(source_bucket)
-    print(source_key)
-    
-    # Read the file content from the source bucket
-    response = s3_client.get_object(Bucket=source_bucket, Key=source_key)
-    #file_content = response['Body'].read().decode('utf-8')
-    try:
-        raw_transcript = response['Body'].read().decode('utf-8')
-    except UnicodeDecodeError as e:
-        raw_transcript = response['Body'].read().decode('utf-8', errors='replace')
-    
-    # takes about 2-3 minutes to run
-    final_essay = full_transcript2essay(raw_transcript)
-    
-    print('Extracting metadata...')
-    metadata = extract_metadata_as_json(final_essay)
-    
-    print("Extracting metadata completed")
-    #print(metadata)
-    
-    
-    
-     # Convert the metadata dictionary to JSON string
-    metadata_json = json.dumps(metadata)
-    
-    # Convert the JSON string to bytes
-    metadata_bytes = metadata_json.encode('utf-8')
-    
+#      #Retrieve the source and destination bucket names from the event
+#     source_bucket = event['Records'][0]['s3']['bucket']['name']
 
-    print(type(metadata_json))
-    print(metadata_json)
-    
-    rst_filepath = r'/tmp/test.rst'
-    json2rst(metadata, rst_filepath)
-    
 
-    destination_key = "transcript_" + source_key.split(".")[0] +".rst"
-    print(destination_key)
-    
-    s3_client.upload_file(rst_filepath, destination_bucket, destination_key)
-    
-  
-    
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Rst file comversion successful !!!')
-    }
+#     # Retrieve the key (filename) of the object that triggered the Lambda function
+#     source_key = event['Records'][0]['s3']['object']['key']
+
+
+#     ACCESS_KEY	= os.environ['ACCESS_KEY'] 
+#     SECRET_KEY	= os.environ['SECRET_KEY'] 
+#     #s3_client = boto3.client('s3')
+#     s3_client = boto3.client(
+#     's3',
+#     aws_access_key_id=ACCESS_KEY,
+#     aws_secret_access_key=SECRET_KEY
+#     )
+
+#     print(source_bucket)
+#     print(source_key)
+
+#     # Read the file content from the source bucket
+#     response = s3_client.get_object(Bucket=source_bucket, Key=source_key)
+#     #file_content = response['Body'].read().decode('utf-8')
+#     try:
+#         raw_transcript = response['Body'].read().decode('utf-8')
+#     except UnicodeDecodeError as e:
+#         raw_transcript = response['Body'].read().decode('utf-8', errors='replace')
+
+#     # takes about 2-3 minutes to run
+#     final_essay = full_transcript2essay(raw_transcript)
+
+#     print('Extracting metadata...')
+#     metadata = extract_metadata_as_json(final_essay)
+
+#     print("Extracting metadata completed")
+#     #print(metadata)
+
+
+
+#      # Convert the metadata dictionary to JSON string
+#     metadata_json = json.dumps(metadata)
+
+#     # Convert the JSON string to bytes
+#     metadata_bytes = metadata_json.encode('utf-8')
+
+
+#     print(type(metadata_json))
+#     print(metadata_json)
+
+#     rst_filepath = r'/tmp/test.rst'
+#     json2rst(metadata, rst_filepath)
+
+
+#     destination_key = "transcript_" + source_key.split(".")[0] +".rst"
+#     print(destination_key)
+
+#     s3_client.upload_file(rst_filepath, destination_bucket, destination_key)
+
+
+
+#     return {
+#         'statusCode': 200,
+#         'body': json.dumps('Rst file comversion successful !!!')
+#     }
