0fc759859c758ee54bcce81b28448a54a39bd8d1
Eyob <eyobyirgu@gmail.com>
2024-05-20 09:24:17 +0300

    Refactor test_usage_tracker.py for clarity and consistency

--- src/tests/integration_tests/test_usage_tracker.py
+++ src/tests/integration_tests/test_usage_tracker.py
@@ -10,34 +10,42 @@ import sqlite3
 
 user_id = "test_id"
 
-def create_testing_db_file(db_path):
-    # Connect to the SQLite database (if it doesn't exist, it will be created)
-    conn = sqlite3.connect(db_path)
-    conn.close()
-    print(f"Database file '{db_path}' created successfully.")
 
-# Specify the path where you want to create the database file
-db_path = "./tests/data/test_usage_counter.db"
-
-# Call the function to create the database file
-create_testing_db_file(db_path)
-
-
-def test_usage_tracker_whitelist_add_and_get():
-    check = os.path.exists("test_usage.db")
-    if check:
+@pytest.fixture
+def db_setup_teardown():
+    if os.path.exists("test_usage.db"):
         os.remove("test_usage.db")
+    yield
+
+    if os.path.exists("test_usage.db"):
+        os.remove("test_usage.db")
+
+
+@pytest.fixture
+def db_setup_s3_bucket():
+
+    db_path = "./tests/data/test_usage_counter.db"
+    if not os.path.exists("./tests/data/test_usage_counter.db"):
+        conn = sqlite3.connect(db_path)
+        conn.close()
+        print(f"Database file '{db_path}' created successfully.")
+    yield
+    if os.path.exists(db_path):
+        os.remove(db_path)
+
+
+def test_usage_tracker_whitelist_add_and_get(db_setup_teardown):
     db = UserUsageTracker(db_name="test_usage.db", db_url="sqlite:///test_usage.db")
     db.add_to_whitelist(user_id=user_id)
     white_listed_id = db.get_all_whitelisted_ids()
     assert user_id in white_listed_id
 
 
-def test_usage_checker():
-    check = os.path.exists("test_usage.db")
-    if check:
-        os.remove("test_usage.db")
-    db = UserUsageTracker(db_name="test_usage.db", db_url="sqlite:///test_usage.db" , )
+def test_usage_checker(db_setup_teardown):
+    db = UserUsageTracker(
+        db_name="test_usage.db",
+        db_url="sqlite:///test_usage.db",
+    )
     db.max_daily_token = 2000
     db.limit_time_size_in_hours = 0.1
     check_usage = db.check_usage(token_amount="3000", user_id="jack")
@@ -50,10 +58,7 @@ def test_usage_checker():
     assert check_usage["can_execute"] == False and check_usage["token-left"] == 0
 
 
-def test_usage_reset_after_a_given_time():
-    check = os.path.exists("test_usage.db")
-    if check:
-        os.remove("test_usage.db")
+def test_usage_reset_after_a_given_time(db_setup_teardown):
     db = UserUsageTracker(db_name="test_usage.db", db_url="sqlite:///test_usage.db")
     db.max_daily_token = 2000
     db.limit_time_size_in_hours = 0.001  # 3.6 seconds
@@ -83,13 +88,14 @@ def mock_download_from_s3(monkeypatch):
         shutil.copyfile(dormant_test_db_path, active_test_db_path)
 
     monkeypatch.setattr(UserUsageTracker, "download_from_s3", mock_download)
-    
+
+
 @pytest.fixture
 def mock_upload_to_s3_from_s3(monkeypatch):
     # by copying and renaming the testing sqlight db file to the location of the dormant file this function mocks uploading to s3 bucket.
     def mock_upload(*args, **kwargs):
         # sqlight db file path which acts as s3 bucket
-        dormant_test_db_path = './tests/data/test_usage_counter.db'
+        dormant_test_db_path = "./tests/data/test_usage_counter.db"
         check = os.path.exists(dormant_test_db_path)
         if check:
             os.remove(dormant_test_db_path)
@@ -103,10 +109,9 @@ def mock_upload_to_s3_from_s3(monkeypatch):
     monkeypatch.setattr(UserUsageTracker, "upload_to_s3", mock_upload)
 
 
-def test_download_db_from_s3(mock_download_from_s3):
-    check = os.path.exists("test_usage.db")
-    if check:
-        os.remove("test_usage.db")
+def test_download_db_from_s3(
+    db_setup_s3_bucket, mock_download_from_s3, db_setup_teardown
+):
     UserUsageTracker.download_from_s3(
         db_name="test_usage.db", db_url="sqlite:///test_usage.db"
     )
@@ -114,12 +119,12 @@ def test_download_db_from_s3(mock_download_from_s3):
     assert third_check, "downloading sqlight file has failed"
 
 
-def test_whitelist_from_s3(mock_download_from_s3 , mock_upload_to_s3_from_s3):
-    check = os.path.exists("test_usage.db")
-    if not check:
-        UserUsageTracker.download_from_s3(
-            db_name="test_usage.db", db_url="sqlite:///test_usage.db"
-        )
+def test_whitelist_from_s3(
+    db_setup_s3_bucket,
+    mock_download_from_s3,
+    mock_upload_to_s3_from_s3,
+    db_setup_teardown,
+):
     db = UserUsageTracker(db_name="test_usage.db", db_url="sqlite:///test_usage.db")
     db.add_to_whitelist(user_id=user_id)
     db.upload_to_s3()
@@ -132,11 +137,7 @@ def test_whitelist_from_s3(mock_download_from_s3 , mock_upload_to_s3_from_s3):
     assert user_id in white_listed_id
 
 
-def test_user_message_logger():
-    check = os.path.exists("test_usage.db")
-    if check:
-        os.remove("test_usage.db")
-
+def test_user_message_logger(db_setup_teardown):
     class TestLogger(BaseVerboseLogger):
         def __init__(self) -> None:
             self.message = ""
